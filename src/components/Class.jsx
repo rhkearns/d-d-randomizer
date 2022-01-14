5cd1666ae6cf39e14ad52f5d6cca253d5ba2b2d0
rhkearns <ryankearns217@gmail.com>
2022-01-14 14:52:17 -0500

    setup and stub components

---
+++ src/components/Class.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Class = () => {
+  return {
+
+  }
+}
+
+export default Class
---
+++ src/components/Race.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Race = () => {
+  return {
+    
+  }
+}
+
+export default Race
