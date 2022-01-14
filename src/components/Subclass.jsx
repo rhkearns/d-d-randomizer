18295bd06e7a32e437b6050b360bc70beee3c159
rhkearns <ryankearns217@gmail.com>
2022-01-14 17:12:30 -0500

    subclass component functional

--- src/components/Subclass.jsx
+++ src/components/Subclass.jsx
@@ -1,13 +1,75 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
+import * as subclassData from '../subclassData'
 
 const Subclass = ({classType, subclass, setSubclass, rng}) => {
   const [subclassArray, setSubclassArray] = useState()
   
-  
+  const fetchSubclassData = (classType) => {
+    console.log('fetching', classType);
+    switch (classType) {
+      case 'Barbarian':
+        setSubclassArray(subclassData.barbarianSubclasses)
+        break;
+      case 'Bard':
+        setSubclassArray(subclassData.bardSubclasses)
+        break;
+      case 'Cleric':
+        setSubclassArray(subclassData.clericSubclasses)
+        break;
+      case 'Druid':
+        setSubclassArray(subclassData.druidSubclasses)
+        break;
+      case 'Fighter':
+        setSubclassArray(subclassData.fighterSubclasses)
+        break;
+      case 'Monk':
+        setSubclassArray(subclassData.monkSubclasses)
+        break;
+      case 'Paladin':
+        setSubclassArray(subclassData.paladinSubclasses)
+        break;
+      case 'Ranger':
+        setSubclassArray(subclassData.rangerSubclasses)
+        break;
+      case 'Rogue':
+        setSubclassArray(subclassData.rogueSubclasses)
+        break;
+      case 'Sorcerer':
+        setSubclassArray(subclassData.sorcererSubclasses)
+        break;
+      case 'Warlock':
+        setSubclassArray(subclassData.warlockSubclasses)
+        break;
+      case 'Wizard':
+        setSubclassArray(subclassData.wizardSubclasses)
+        break;
+      case 'Artificer':
+        setSubclassArray(subclassData.artificerSubclasses)
+        break;
+      default:
+        break;
+    }
+  }
+
+  const genRandSubclass = () => {
+    console.log('here', subclassArray);
+    let idx = rng(subclassArray.length)
+    console.log(subclassArray[idx]);
+    setSubclass(subclassArray[idx])
+    console.log('subclass', subclass);
+  }
+
+  useEffect(() => {
+    fetchSubclassData(classType)
+  }, [classType])
 
 
   return (
-    <h1>Subclass</h1>
+    <>
+      <h2>Subclass</h2>
+      <h1>{subclass}</h1>
+      <button onClick={() => genRandSubclass()}>Randomize Subclass</button>
+    </>
   )
 }
 
