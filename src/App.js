7840fada41613231301d71a5bc906f00387972c6
rhkearns <ryankearns217@gmail.com>
2022-01-14 15:37:09 -0500

    create rng function

--- src/App.js
+++ src/App.js
@@ -5,15 +5,21 @@ import Race from './components/Race';
 import Subclass from './components/Subclass';
 
 function App() {
-  const [character, setCharacter]= useState({})
+  const [character, setCharacter]= useState({
+    'Race': '',
+    'Class': '',
+    'Subclass': ''
+  })
   
-
+  function rng(length) {
+    return Math.floor(Math.random() * (length - 0) + 0)
+  }
 
   return (
     <div className="App">
       <h1>D&D Randomizer</h1>
       <h3>Create A Random Character for Dungeons and Dragons</h3>
-      <Race />
+      <Race character={character} setCharacter={setCharacter} rng={rng}/>
       <Class />
       <Subclass />
     </div>
